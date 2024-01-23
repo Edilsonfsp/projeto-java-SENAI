3d10e813ead7ddce27c96728b28270f3977a17e9
Edilson Souza <edilsonfsp@gmail.com>
2024-01-23 10:02:52 -0300

    Criando os metodos crud da classe UserResource

--- src/main/java/com/edilson/projeto/resources/UserResource.java
+++ src/main/java/com/edilson/projeto/resources/UserResource.java
@@ -1,13 +1,19 @@
 package com.edilson.projeto.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.edilson.projeto.entities.User;
 import com.edilson.projeto.services.UserService;
@@ -17,7 +23,15 @@ import com.edilson.projeto.services.UserService;
 public class UserResource {
 
 	@Autowired
-    private UserService service;
+    private UserService service;    
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+		
+		User newObj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+    }
     
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
@@ -32,5 +46,18 @@ public class UserResource {
 		List<User> list = this.service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable Integer id) {
+        service.delete(id);
+    }
+
+    @PutMapping(value = "/{id}")
+    public void update(@RequestBody User user, @PathVariable Integer id) {
+
+        // Certifique-se de que o objeto 'user' contenha todas as informações necessárias para a atualização.
+        service.update(user, id);
+
+    }
 
 }
