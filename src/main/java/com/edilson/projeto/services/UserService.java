d8a473e1ba18a18c13e8a50ba3ca247072036499
Edilson Souza <edilsonfsp@gmail.com>
2024-01-23 10:03:25 -0300

    Criando os metodos crud da classe UserService

--- src/main/java/com/edilson/projeto/services/UserService.java
+++ src/main/java/com/edilson/projeto/services/UserService.java
@@ -18,15 +18,39 @@ public class UserService {
 
     public User findById(Integer id) {
 		
-		Optional<User> obj = repository.findById(id);
-		//return obj.orElse(null);
-        return obj.orElseThrow(
+		Optional<User> newObj = repository.findById(id);
+		//return newObj.orElse(null);
+        return newObj.orElseThrow(
             () -> new ObjectNotFoundException("Objeto não encontrado " + id + ", Tipo: " + User.class.getName())
             );
     }
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public void delete(Integer id) {
+
+		findById(id);
+		repository.deleteById(id);
+        
+    }
+
+    public User update(User user, Integer id) {
+		User newObj = findById(id);
+		newObj.setNome(newObj.getNome());
+		newObj.setEmail(newObj.getEmail());
+		newObj.setPassword(newObj.getPassword());
+		return repository.save(newObj);	
+
+    }
+
+    public User create(User obj) {
+
+		obj.setId(null);
+		
+		return repository.save(obj);
+        
     }
     
 }
